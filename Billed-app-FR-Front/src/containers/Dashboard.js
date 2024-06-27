e9a3e19dbdb9624558160ed1f73124194dcc7158
Ben Bld <bld.prom@gmail.com>
2024-06-28 00:27:06 +0200

    fix : admin dashboard issue (had to click twice on the chevron to close open lists)

--- Billed-app-FR-Front/src/containers/Dashboard.js
+++ Billed-app-FR-Front/src/containers/Dashboard.js
@@ -133,13 +133,18 @@ export default class {
   handleShowTickets(e, bills, index) {
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
+    if (this.counter % 2 === 0 && $(`#arrow-icon${this.index}`).attr('data-open') === 'true') {
+      this.counter++
+    }
     if (this.counter % 2 === 0) {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(0deg)' })
+      $(`#arrow-icon${this.index}`).attr('data-open', 'true')
       $(`#status-bills-container${this.index}`)
         .html(cards(filteredBills(bills, getStatus(this.index))))
       this.counter++
     } else {
       $(`#arrow-icon${this.index}`).css({ transform: 'rotate(90deg)' })
+      $(`#arrow-icon${this.index}`).attr('data-open', 'false')
       $(`#status-bills-container${this.index}`)
         .html("")
       this.counter++
